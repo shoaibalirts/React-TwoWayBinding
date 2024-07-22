580d2198f0614c9dcec79621da364f3a30800832
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-22 17:02:54 +0200

    Assignment completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import Review from "./Review";
+import { useState } from "react";
 // don't change the Component name "App"
+
 function App() {
+  const [feedback, setFeedback] = useState();
+  const [teacherName, setTeacherName] = useState();
+  const feedbackHandleChange = (event) => {
+    setFeedback(event.target.value);
+  };
+  const nameHandleChange = (event) => {
+    setTeacherName(event.target.value);
+  };
   return (
     <>
       <section id="feedback">
         <h2>Please share some feedback</h2>
         <p>
           <label>Your Feedback</label>
-          <textarea />
+          <textarea onChange={feedbackHandleChange} value={feedback} />
         </p>
         <p>
           <label>Your Name</label>
-          <input type="text" />
+          <input type="text" onChange={nameHandleChange} value={teacherName} />
         </p>
       </section>
       <section id="draft">
         <h2>Your feedback</h2>
 
-        <Review />
+        <Review feedback={feedback} student={teacherName} />
 
         <p>
           <button>Save</button>
